d985d1d84b6593c245ec3525d95f80da89d98cbc
Kevin Leung <kevinresol@gmail.com>
2018-02-12 16:40:35 +0800

    Fix build

--- src/lix/server/util/Password.hx
+++ src/lix/server/util/Password.hx
@@ -5,7 +5,7 @@ using tink.CoreApi;
 @:build(futurize.Futurize.build())
 class Password {
   public static function encrypt(raw:String, salt:String, iterations:Int) {
-    #if tests iterations = iterations >> 8 #end; // make it faster in tests
+    #if tests iterations = iterations >> 8; #end // make it faster in tests
     return @:futurize js.node.Crypto.pbkdf2(raw, salt, iterations, 512, 'sha512', $cb1)
       .next(function(buffer) return buffer.toString('hex'));
   }
