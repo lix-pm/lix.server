58e23038d7b020c4a740bee5177168b58bbd959f
Kevin Leung <kevinresol@gmail.com>
2018-10-02 00:19:18 +0800

    Add OAuthHelper

---
+++ lib/lix/util/OAuthHelper.hx
@@ -0,0 +1,72 @@
+package lix.util;
+
+import lix.util.Config.*;
+import tink.http.Client.*;
+import tink.http.Header;
+import tink.Chunk;
+
+using tink.CoreApi;
+
+class OAuthHelper {
+  static var COGNITO_CLIENT_SECRET = Sys.getEnv('COGNITO_CLIENT_SECRET');
+  
+  public static function authorize(?state:String, type:ResponseType, redirect:String):tink.Url {
+    return 'https://lix.auth.us-east-2.amazoncognito.com/oauth2/authorize?' + 
+      tink.QueryString.build({
+        response_type: type,
+        client_id: COGNITO_CLIENT_ID,
+        state: state,
+        redirect_uri: redirect,
+      });
+  }
+  
+  public static function callback(code:String, redirect:String, ?state:String):Promise<TokenResponse> {
+    return fetch('https://lix.auth.us-east-2.amazoncognito.com/oauth2/token', {
+      method: POST,
+      headers: [
+        new HeaderField(AUTHORIZATION, HeaderValue.basicAuth(COGNITO_CLIENT_ID, COGNITO_CLIENT_SECRET)),
+        new HeaderField(CONTENT_TYPE, 'application/x-www-form-urlencoded'),
+        new HeaderField(ACCEPT, 'application/json'),
+      ],
+      body: tink.QueryString.build({
+        grant_type: 'authorization_code',
+        client_id: COGNITO_CLIENT_ID,
+        redirect_uri: redirect,
+        code: code,
+      }),
+    }).all()
+      .next(res -> tink.Json.parse((res.body:TokenResponse)));
+  }
+  
+  public static function getUserInfo(accessToken:String) {
+    return fetch('https://lix.auth.us-east-2.amazoncognito.com/oauth2/userInfo', { 
+        headers: [new HeaderField(AUTHORIZATION, 'Bearer $accessToken')],
+    }).all().next(res -> tink.Json.parse((res.body:UserInfo)));
+  }
+}
+
+typedef TokenResponse = {
+  access_token:String,
+  ?refresh_token:String,
+  token_type:String,
+  id_token:String,
+  expires_in:Int,
+}
+
+typedef UserInfo = {
+  sub:String,
+  website:String,
+  email_verified:String,
+  updated_at:String,
+  profile:String,
+  name:String,
+  email:String,
+  picture:String,
+  username:String,
+}
+
+@:enum
+abstract ResponseType(String) to String {
+  var ImplicitCodeGrant = 'token';
+  var AuthorizationCodeGrant = 'code';
+}
--- src/lix/server/api/OAuthApi.hx
+++ src/lix/server/api/OAuthApi.hx
@@ -2,77 +2,16 @@ package lix.server.api;
 
 import lix.api.OAuthApi;
 import lix.util.Config.*;
-import tink.http.Fetch.fetch;
+import lix.util.OAuthHelper;
 
 class OAuthApi extends BaseApi implements lix.api.OAuthApi {
   
-  static var COGNITO_CLIENT_SECRET = Sys.getEnv('COGNITO_CLIENT_SECRET');
-  
   public function authorize(?state:String):tink.Url {
-    return 'https://lix.auth.us-east-2.amazoncognito.com/oauth2/authorize?' + 
-      tink.QueryString.build({
-        response_type: 'code',
-        client_id: COGNITO_CLIENT_ID,
-        state: state,
-        redirect_uri: '$API_SERVER_URL/oauth2/callback',
-      });
+    return OAuthHelper.authorize(state, AuthorizationCodeGrant, '$API_SERVER_URL/oauth2/callback');
   }
   
   public function callback(code:String, ?state:String, ?action:String):Promise<OutgoingResponse> {
-    trace(action);
-    return fetch('https://lix.auth.us-east-2.amazoncognito.com/oauth2/token', {
-      method: POST,
-      headers: [
-        new HeaderField(AUTHORIZATION, HeaderValue.basicAuth(COGNITO_CLIENT_ID, COGNITO_CLIENT_SECRET)),
-        new HeaderField(CONTENT_TYPE, 'application/x-www-form-urlencoded'),
-        new HeaderField(ACCEPT, 'application/json')
-      ],
-      body: tink.QueryString.build({
-        grant_type: 'authorization_code',
-        client_id: COGNITO_CLIENT_ID,
-        redirect_uri: '$API_SERVER_URL/oauth2/callback',
-        code: code,
-      })
-    }).all()
-      .next(res -> tink.Json.parse((res.body:TokenResponse)))
-      // .next(token -> getUserInfo(token.access_token).swap(token))
+    return OAuthHelper.callback(code, state, '$API_SERVER_URL/oauth2/callback')
       .next(token -> new Error('TODO'));
   }
-  
-  static function redirect(action:OAuthAction, query:String) {
-    return new OutgoingResponse(
-      new ResponseHeader(302, 'Found', [new HeaderField(LOCATION, '$SITE_URL/$action?$query')]),
-      tink.io.Source.EMPTY
-    );
-  }
-  
-  static function getUserInfo(accessToken:String) {
-    return fetch('https://lix.auth.us-east-2.amazoncognito.com/oauth2/userInfo', { 
-        headers: [new HeaderField(AUTHORIZATION, 'Bearer $accessToken')],
-    }).all().next(res -> tink.Json.parse((res.body:UserInfo)));
-  }
-  
-  static function randomString()
-    return haxe.crypto.Sha1.encode('${Date.now().getTime()}${Math.random()}');
-    
-}
-
-private typedef TokenResponse = {
-  access_token:String,
-  refresh_token:String,
-  token_type:String,
-  id_token:String,
-  expires_in:Int,
-}
-
-private typedef UserInfo = {
-  sub:String,
-  website:String,
-  email_verified:String,
-  updated_at:String,
-  profile:String,
-  name:String,
-  email:String,
-  picture:String,
-  username:String,
 }
