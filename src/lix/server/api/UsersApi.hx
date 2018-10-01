c23d9cabe478f95a1b1bfdb8ca3043292c880026
Kevin Leung <kevinresol@gmail.com>
2018-10-02 01:36:49 +0800

    Minor

--- src/lix/server/api/UsersApi.hx
+++ src/lix/server/api/UsersApi.hx
@@ -4,12 +4,13 @@ import lix.api.UsersApi;
 
 class UsersApi extends BaseApi implements lix.api.UsersApi {
   public function create(body:NewUserData, user:lix.api.auth.AuthUser):Promise<User> {
+    // TODO: validate username
     return db.User.insertOne({
       id: null,
       cognitoId: user.id,
       username: body.username,
       nickname: body.nickname,
-    }).next(id -> db.User.where(User.id == id).first());
+    }).next(_ -> db.User.where(User.username == body.username).first());
   }
   
   public function byName(username:String) 
